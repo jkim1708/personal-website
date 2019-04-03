3a9e63298904427b327fd1379ed1b04e6cbba254
Josef Kim <jkim1708@gmail.com>
2019-04-03 14:02:48 +0200

    test123

--- src/main/main.go
+++ src/main/main.go
@@ -83,6 +83,7 @@ func renderTemplate(w http.ResponseWriter, tmpl string) {
 func renderDynamicTemplate(w http.ResponseWriter, tmpl string, data interface{}) {
 	err := templates.ExecuteTemplate(w, tmpl+".html", data)
 	if err != nil {
+
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 }
